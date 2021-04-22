b964e693514ec66f2d98c9897e01539f450f9b08
anwarmontasir <anwarmontasir@gmail.com>
2021-04-22 13:57:39 -0700

    update quotes

--- js/script.js
+++ js/script.js
@@ -13,12 +13,12 @@ project 1 - A Random Quote Generator
 
 const quotes = [
   {quote: 'Every great developer you know got there by solving problems they were unqualified to solve until they actually did it.', source: 'Patrick McKenzie', citation: 'Twitter', url: 'https://twitter.com/codewisdom/status/1182702520696803329?lang=en'},
-  {quote: 'Well, nobody’s perfect.', source: 'Osgood Fielding III', citation: 'Some Like It Hot', url: 'https://www.url.com/title/tt0053291/'},
-  {quote: 'Do your parents know you’re Ramones?', source: 'Miss Togar', citation: 'Rock N Roll High School', url: 'https://www.url.com/title/tt0079813/'}, 
-  {quote: 'No Christine, that’s a frog. Bears wear hats.', source: 'Jim Henson', citation: 'The Great Muppet Caper', url: 'https://www.url.com/title/tt0082474/'},
-  {quote: 'IS THIS SOMETHING YOU CAN SHARE WITH THE REST OF US, AMAZING LARRY?', source: 'Pee Wee Herman', citation: 'Pee Wee’s Big Adventure', url: 'https://www.url.com/title/tt0089791/'}, 
-  {quote: 'And what better way to say “I love you” than with the gift of a spatula?', source: '“Weird Al” Yankovic', citation: 'UHF', url: 'https://www.url.com/title/tt0098546/'}, 
-  {quote: 'Well, what if there is no tomorrow? There wasn’t one today.', source: 'Phil Connors', citation: 'Groundhog Day', url: 'https://www.url.com/title/tt0107048/'}
+  {quote: 'Injustice anywhere is a threat to justice everywhere.', source: 'Martin Luther King, Jr.', citation: 'Letter From a Birmingham Jail', url: 'https://mlk50.civilrightsmuseum.org/justice'},
+  {quote: 'One child,one teacher,one book and one pen can change the world', source: 'Malala Yousafzai', citation: 'United Nations Youth Assembly', url: 'https://www.un.org/youthenvoy/video/malala-yousafzai-addresses-united-nations-youth-assembly/'}, 
+  {quote: 'My silences had not protected me. Your silence will not protect you.', source: 'Audre Lorde', citation: 'Your Silences Will Not Protect You', url: 'https://www.theguardian.com/books/2017/oct/04/your-silence-will-not-protect-you-by-audre-lorder-review'},
+  {quote: 'It’s enough for me to be sure that you and I exist at this moment.', source: 'Gabriel Garcia Márquez', citation: 'One Hundred Years Of Solitude', url: 'https://en.wikipedia.org/wiki/One_Hundred_Years_of_Solitude'}, 
+  {quote: 'I think everybody should be allowed to be who they are, and to love who they love.', source: 'Dolly Parton', citation: 'Billboard', url: 'https://www.billboard.com/articles/columns/the-615/6296620/dolly-parton-talks-50-years-in-nashville-and-supporting-gay-fans'}, 
+  {quote: 'Everybody, try laughing. Then whatever scares you will go away.', source: 'Tatsuo Kusakabe', citation: 'My Neighbor Totoro', url: 'https://medium.com/@mattjones012/totoro-in-uncertain-times-53bb0e7a030c'}
 ];
 
 /* bg array */
